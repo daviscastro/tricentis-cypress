8bf6ecbf0b61d73a28b4edfb248733ef8847affa
Davis Robert <77130887+daviscastro@users.noreply.github.com>
2022-09-02 17:23:54 -0300

    Update sform.spec.js

--- cypress/integration/sform.spec.js
+++ cypress/integration/sform.spec.js
@@ -1,7 +1,7 @@
 import SignupPage from '../pages/SignupPage'
 import signupFactory from '../factories/SignupFactory'
 
-describe('Automobile Insurance Alterando e Fazendo Commit pela segunda vez', ()=> {
+describe('Automobile Insurance Alterando e Fazendo Commit pela segunda vez e fazendo update', ()=> {
 
    
     // it('Enter Vehicle Data,', function () {
